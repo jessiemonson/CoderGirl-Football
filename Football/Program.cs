95e6d37a05b9cd6b3e411f65dcdd7a19fcac3cd5
Jessie Monson <jessiemonson1980@gmail.com>
2019-02-20 22:06:07 -0600

    football final

--- Football/Program.cs
+++ Football/Program.cs
@@ -9,13 +9,13 @@ namespace Football
             Console.Write("Enter the number of touchdowns(7 points): ");
             int touchdowns = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter the number of field goals(3 points: ");
+            Console.Write("Enter the number of field goals(3 points): ");
             int fieldgoals = int.Parse(Console.ReadLine());
 
             // Compute the number of points in a single line of code.
-            int points = 0;
-
-            Console.WriteLine($"The team scored {points} points.");
+            int points = (touchdowns * 7) + (fieldgoals * 3);
+            
+            Console.WriteLine($"The team scored {points.ToString()} points.");
 
             Console.ReadLine();
         }
